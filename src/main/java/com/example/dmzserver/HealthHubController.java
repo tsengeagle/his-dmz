1eca7cccfb3a8fc1466ee48984cdb0ba7736b15d
曾義格 <tsengeagle@tzuchi.com.tw>
2022-09-27 14:27:41 +0800

    request先解析、驗證，再過水給NIS，加trace id

--- src/main/java/com/example/dmzserver/HealthHubController.java
+++ src/main/java/com/example/dmzserver/HealthHubController.java
@@ -6,24 +6,31 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeParseException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api")
 public class HealthHubController {
 
-    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+    private final Logger                  logger       = LoggerFactory.getLogger(this.getClass());
     private final HashMap<String, String> vitalSignTypeMap;
+    private final RestTemplate restTemplate = new RestTemplate();
+    private final String       nisUrl       = "http://10.2.105.117:8180/NisServer/api/handover/healthHubUploadData";
 
     public HealthHubController() {
-        vitalSignTypeMap = new HashMap();
+        vitalSignTypeMap = new HashMap<>();
         vitalSignTypeMap.put("BP", "血壓");
         vitalSignTypeMap.put("BS", "血糖");
         vitalSignTypeMap.put("OX", "血氧");
@@ -34,7 +41,9 @@ public class HealthHubController {
 
     @PostMapping("/FS/uploaddata")
     public ResponseEntity healthHubUploadData(@RequestBody Map inputData) {
-        logger.info("receive input data: {}", inputData);
+        UUID uuid = UUID.randomUUID();
+        logger.info("receive input data: {}, trace id: {}", inputData, uuid.toString());
+
         String decoded = ASUSDecrypt.DecrypeData((String) inputData.get("data"));
         logger.info("decoded data: {}", decoded);
 
@@ -52,8 +61,20 @@ public class HealthHubController {
         }
 
         logger.info("收到正確的資料: {}", data.toString());
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add("request-id", uuid.toString());
 
-        return ResponseEntity.ok("done");
+        HttpEntity<Map>        requestEntity  = new HttpEntity<>(inputData, httpHeaders);
+        ResponseEntity<String> responseEntity = null;
+        try {
+            responseEntity = restTemplate.postForEntity(nisUrl, requestEntity, String.class);
+            logger.info("call nis status: {}, response: {}, trace id: {}", responseEntity.getStatusCode(), responseEntity.getBody(), uuid.toString());
+        } catch (RestClientException e) {
+            logger.error("trace id: {}, call nis fail: {}", uuid.toString(), e.getMessage());
+            return ResponseEntity.badRequest().body("call nis fail, trace id: " + uuid.toString());
+        }
+
+        return responseEntity;
     }
 
     @PostMapping("/FS/demo")
